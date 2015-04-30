3dddb7953441470955e21afa76f220696b14d03b
Weiming <WWang33@slb.com>
2015-04-30 21:54:59 +0800

    Add Toc.json

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('This would be the main JS file.');
+(function(){
+    'use strict';
+    console.log('app lunch');
+
+    var blogModule = angular.module('blog', []);
+
+    blogModule.controller('NavController', ['$scope', '$http', function($scope, $http){
+        $scope.tags = [];
+        $scope.postUnder = function(tag){
+            console.log('reader interested in: ' + tag);
+        };
+        
+        $http
+            .get('posts/toc.json')
+            .then(function(result){
+                if(result.status == 200){
+                    $scope.tags = result.data.tags;
+                }
+            });
+    }]);
+    
+})();
+
